7a76118087914e8f6dedf35f2ae8dcb2e60d51dc
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-10-12 19:50:51 -0600

    Agregando una ruta dummy

--- src/routers/comment.js
+++ src/routers/comment.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/helloworld", function (request, response) {
+	response.send("Hello World");
+});
+
 module.exports = router;
